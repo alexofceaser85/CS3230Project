cb2995c272db32435beb798409738ec1dda01c93
Alex Decesare <adecesa1@my.westga.edu>
2022-09-19 21:34:23 -0400

    Add login implimentation
    Adds the login page UI
    Adds the layers from the DAL to the UI (model and viewmodel)

--- CS3230Project/DAL/Accounts/AccountsDal.cs
+++ CS3230Project/DAL/Accounts/AccountsDal.cs
@@ -23,32 +23,32 @@ namespace CS3230Project.DAL.Accounts
         /// </summary>
         /// <param name="userName">The username to log in</param>
         /// <param name="password">The password to log in</param>
-        public static void Login(string userName, string password)
+        public static bool Login(string userName, string password)
         {
             if (userName == null)
             {
-                throw new ArgumentException(AccountsDalErrorMessages.LoginUserNameCannotBeNull);
+                throw new ArgumentException(AccountsErrorMessages.LoginUserNameCannotBeNull);
             }
 
             if (userName.Trim().Length == 0)
             {
-                throw new ArgumentException(AccountsDalErrorMessages.LoginUserNameCannotBeEmpty);
+                throw new ArgumentException(AccountsErrorMessages.LoginUserNameCannotBeEmpty);
             }
 
             if (password == null)
             {
-                throw new ArgumentException(AccountsDalErrorMessages.LoginPasswordCannotBeNull);
+                throw new ArgumentException(AccountsErrorMessages.LoginPasswordCannotBeNull);
             }
 
             if (password.Trim().Length == 0)
             {
-                throw new ArgumentException(AccountsDalErrorMessages.LoginPasswordCannotBeEmpty);
+                throw new ArgumentException(AccountsErrorMessages.LoginPasswordCannotBeEmpty);
             }
 
             using var connection = new MySqlConnection(Connection.ConnectionString);
             connection.Open();
 
-            const string query = "select firstName, lastName, ID, userName from nurses, accounts where nurses.ID = accounts.employeeID and exists (select userName, `password` from accounts where userName = @username and `password` = @password);";
+            const string query = "select firstName, lastName, ID, userName from nurses, accounts where nurses.ID = accounts.employeeID and exists (select userName, `password` from accounts where userName = @username and `password` = @password and accounts.employeeID = nurses.ID);";
             using var command = new MySqlCommand(query, connection);
             command.Parameters.Add("@userName", MySqlDbType.String).Value = userName;
             command.Parameters.Add("@password", MySqlDbType.String).Value = password;
@@ -59,14 +59,16 @@ namespace CS3230Project.DAL.Accounts
             var lastNameOrdinal = reader.GetOrdinal("lastName");
             var userNameOrdinal = reader.GetOrdinal("userName");
 
-            while (reader.Read())
-            {
+            while (reader.Read()) 
+            { 
                 CurrentUser.User = new Nurse(
                     idOrdinal, 
                     reader.GetFieldValueCheckNull<string>(firstNameOrdinal), 
                     reader.GetFieldValueCheckNull<string>(lastNameOrdinal), 
                     reader.GetFieldValueCheckNull<string>(userNameOrdinal));
             }
+
+            return CurrentUser.User != null;
         }
     }
 }
